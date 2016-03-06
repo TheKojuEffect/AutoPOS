02a450bfa3d5da93a2970d21be95a50c538a8811
Kapil Koju <kplx2.0@gmail.com>
2016-03-06 22:37:28 +0545

    Add a test to check item code in DB and application to be consistent. Closes #5

--- src/test/groovy/io/koju/autopos/catalog/service/ItemCodeUtilTest.groovy
+++ src/test/groovy/io/koju/autopos/catalog/service/ItemCodeUtilTest.groovy
@@ -1,8 +1,18 @@
 package io.koju.autopos.catalog.service
 
+import io.koju.autopos.Application
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.web.WebAppConfiguration
 import spock.lang.Specification
 
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
 class ItemCodeUtilTest extends Specification {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate
 
     def "ItemCode getId and getCode process is working and symmetric"() {
 
@@ -21,5 +31,23 @@ class ItemCodeUtilTest extends Specification {
         5635254252L          || 'KPGYMFCC'
         99427982344L         || 'XRNSKRXR'
         9223372036854775807L || 'TCWHHMFDCBX3XH'
+    }
+
+    def "ItemCode.getCode is consistent with item_code function in DB"() {
+
+        expect:
+        ItemCodeUtil.getCode(id) == jdbcTemplate.queryForObject("SELECT item_code(?);", String, id)
+
+        where:
+        id                   | _
+        1L                   | _
+        22L                  | _
+        1000L                | _
+        10272L               | _
+        231132L              | _
+        5635254252L          | _
+        994272982344L        | _
+        9223372036854775807L | _
+
     }
 }
