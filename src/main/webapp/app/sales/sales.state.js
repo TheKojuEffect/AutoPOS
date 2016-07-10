21354fbf915de76543a5fed80a24ddd1bbc234a4
Kapil Koju <kplx2.0@gmail.com>
2016-07-10 11:51:02 +0545

    Fix page title for sales

--- src/main/webapp/app/sales/sales.state.js
+++ src/main/webapp/app/sales/sales.state.js
@@ -1,7 +1,10 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('autopos')
-    .config(function ($stateProvider) {
+    angular.module('autopos')
+        .config(stateConfig);
+
+    function stateConfig($stateProvider) {
         $stateProvider
             .state('sales', {
                 abstract: true,
@@ -18,7 +21,7 @@ angular.module('autopos')
                 url: '/sales/pending?page&sort&search',
                 data: {
                     authorities: ['ROLE_USER'],
-                    pageTitle: 'Pending Sales'
+                    pageTitle: 'autopos.sales.pendingSales'
                 },
                 views: {
                     'content-tab': {
@@ -52,55 +55,57 @@ angular.module('autopos')
                         };
                     }],
                     translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                        $translatePartialLoader.addPart('item');
+                        $translatePartialLoader.addPart('sales');
                         $translatePartialLoader.addPart('global');
                         return $translate.refresh();
                     }]
                 }
             })
             .state('sales.completed', {
-            parent: 'sales',
-            url: '/sales/completed?page&sort&search',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'Completed Sales'
-            },
-            views: {
-                'content-tab': {
-                    templateUrl: 'app/sales/saleList.html',
-                    controller: 'SaleListCtrl',
-                    controllerAs: 'vm'
+                parent: 'sales',
+                url: '/sales/completed?page&sort&search',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'autopos.sales.completedSales'
+                },
+                views: {
+                    'content-tab': {
+                        templateUrl: 'app/sales/saleList.html',
+                        controller: 'SaleListCtrl',
+                        controllerAs: 'vm'
+                    }
+                },
+                params: {
+                    page: {
+                        value: '1',
+                        squash: true
+                    },
+                    sort: {
+                        value: 'id,asc',
+                        squash: true
+                    },
+                    search: null
+                },
+                resolve: {
+                    saleStatus: function () {
+                        return 'COMPLETED';
+                    },
+                    pagingParams: function ($stateParams, PaginationUtil) {
+                        return {
+                            page: PaginationUtil.parsePage($stateParams.page),
+                            sort: $stateParams.sort,
+                            predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                            ascending: PaginationUtil.parseAscending($stateParams.sort),
+                            search: $stateParams.search
+                        };
+                    },
+                    translatePartialLoader: function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('sales');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }
                 }
-            },
-            params: {
-                page: {
-                    value: '1',
-                    squash: true
-                },
-                sort: {
-                    value: 'id,asc',
-                    squash: true
-                },
-                search: null
-            },
-            resolve: {
-                saleStatus: function () {
-                    return 'COMPLETED';
-                },
-                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
-                    return {
-                        page: PaginationUtil.parsePage($stateParams.page),
-                        sort: $stateParams.sort,
-                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
-                        ascending: PaginationUtil.parseAscending($stateParams.sort),
-                        search: $stateParams.search
-                    };
-                }],
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('item');
-                    $translatePartialLoader.addPart('global');
-                    return $translate.refresh();
-                }]
-            }
-        });
-    });
+            });
+    }
+
+})();
