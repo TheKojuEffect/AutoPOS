f5f64a6ac0009de5c14d6e8db815bb0af2b2ae72
Kapil Koju <kplx2.0@gmail.com>
2016-07-12 07:10:45 +0545

    Remove unnecessary comments

--- src/main/java/io/koju/autopos/catalog/web/ItemResource.java
+++ src/main/java/io/koju/autopos/catalog/web/ItemResource.java
@@ -27,9 +27,6 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * REST controller for managing Item.
- */
 @RestController
 @RequestMapping("/api")
 @Slf4j
@@ -41,13 +38,7 @@ public class ItemResource {
     ItemResource(ItemService itemService) {
         this.itemService = itemService;
     }
-    /**
-     * POST  /items : Create a new item.
-     *
-     * @param item the item to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new item, or with status 400 (Bad Request) if the item has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
+
     @RequestMapping(value = "/items",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -63,15 +54,6 @@ public class ItemResource {
                 .body(result);
     }
 
-    /**
-     * PUT  /items : Updates an existing item.
-     *
-     * @param item the item to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated item,
-     * or with status 400 (Bad Request) if the item is not valid,
-     * or with status 500 (Internal Server Error) if the item couldnt be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
     @RequestMapping(value = "/items",
             method = RequestMethod.PUT,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -87,31 +69,19 @@ public class ItemResource {
                 .body(result);
     }
 
-    /**
-     * GET  /items : get all the items.
-     *
-     * @param pageable the pagination information
-     * @return the ResponseEntity with status 200 (OK) and the list of items in body
-     * @throws URISyntaxException if there is an error to generate the pagination HTTP headers
-     */
     @RequestMapping(value = "/items",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
     public ResponseEntity<List<Item>> getAllItems(Pageable pageable, ItemFilter itemFilter)
-        throws URISyntaxException {
+            throws URISyntaxException {
         log.debug("REST request to get a page of Items");
         Page<Item> page = itemService.findAll(itemFilter, pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/items");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
 
-    /**
-     * GET  /items/:id : get the "id" item.
-     *
-     * @param id the id of the item to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the item, or with status 404 (Not Found)
-     */
+
     @RequestMapping(value = "/items/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -126,12 +96,6 @@ public class ItemResource {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    /**
-     * DELETE  /items/:id : delete the "id" item.
-     *
-     * @param id the id of the item to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
     @RequestMapping(value = "/items/{id}",
             method = RequestMethod.DELETE,
             produces = MediaType.APPLICATION_JSON_VALUE)
