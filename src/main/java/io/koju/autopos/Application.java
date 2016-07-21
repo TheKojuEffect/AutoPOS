0478f4422ada35474fdee101ccdc59594f089122
Kapil Koju <kplx2.0@gmail.com>
2016-07-21 10:06:29 +0545

    Add missing import

--- src/main/java/io/koju/autopos/Application.java
+++ src/main/java/io/koju/autopos/Application.java
@@ -14,6 +14,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
