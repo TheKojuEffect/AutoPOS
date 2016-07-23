9b3378744a4e275b044c4d7e0bfb40d2e6c54d02
Kapil Koju <kplx2.0@gmail.com>
2016-07-23 08:09:01 +0545

    Remove date from saleLineEntry for now. Closes #32

--- src/main/webapp/app/sales/saleLineEntry.js
+++ src/main/webapp/app/sales/saleLineEntry.js
@@ -1,25 +1,13 @@
-(function () {
-    angular.module('autopos')
-        .component('saleLineEntry', {
-            templateUrl: 'app/sales/saleLineEntry.html',
-            controller: ItemEntryCtrl
-        });
+class ItemEntryCtrl {
 
-    function ItemEntryCtrl() {
-        var ctrl = this;
-        ctrl.datePickerOptions = {
-            maxDate: new Date()
-        };
-        ctrl.datePickerOpenStatus = {};
-        ctrl.datePickerOpenStatus.date = false;
-
-        ctrl.openCalendar = function (date) {
-            ctrl.datePickerOpenStatus[date] = true;
-        };
-        
-        ctrl.itemSelected = function(item) {
-            console.log(item);
-        }
+    itemSelected(item) {
+        console.log(item);
     }
+}
 
-})();
+angular.module('autopos')
+    .component('saleLineEntry', {
+        templateUrl: 'app/sales/saleLineEntry.html',
+        controller: ItemEntryCtrl
+    });
+
