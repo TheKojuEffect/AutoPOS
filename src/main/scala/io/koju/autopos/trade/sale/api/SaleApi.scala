136355ed956e07bd8aaa80aba080cb0b7782ea93
Kapil Koju <kplx2.0@gmail.com>
2016-08-30 22:10:18 +0545

    Move sale line api to SaleLineApi
    Closes #55

--- src/main/scala/io/koju/autopos/trade/sale/api/SaleApi.scala
+++ src/main/scala/io/koju/autopos/trade/sale/api/SaleApi.scala
@@ -54,42 +54,5 @@ class SaleApi(private val saleService: SaleService,
     }
   }
 
-  @GetMapping(Array("/{saleId}/lines/{saleLineId}"))
-  @Timed
-  def getSaleLine(@PathVariable("saleId") saleId: Long,
-                  @PathVariable("saleLineId") saleLineOp: SaleLine): ResponseEntity[SaleLine] = {
-    Option(saleLineOp) match {
-      case Some(saleLine)
-        if saleLine.getSale.getId == saleId => ResponseEntity.ok(saleLine)
-      case _ => new ResponseEntity[SaleLine](HttpStatus.NOT_FOUND)
-    }
-  }
 
-
-  @PostMapping(Array("/{saleId}/lines"))
-  @Timed
-  def getSaleLine(@PathVariable("saleId") sale: Sale,
-                  @RequestBody @Valid saleLine: SaleLine): ResponseEntity[SaleLine] = {
-
-    saleLine.setSale(sale)
-    saleLine.setId(null)
-    saleLineRepo.save(saleLine)
-    ResponseEntity
-      .created(new URI(s"/api/sales/${sale.getId}/lines/${saleLine.getId}"))
-      .body(saleLine)
-  }
-
-  @PutMapping(Array("/{saleId}/lines/{saleLineId}"))
-  @Timed
-  def getSaleLine(@PathVariable("saleId") sale: Sale,
-                  @PathVariable("saleLineId") saleLineId: Long,
-                  @RequestBody @Valid saleLine: SaleLine): ResponseEntity[SaleLine] = {
-
-    saleLine.setSale(sale)
-    saleLine.setId(saleLineId)
-    saleLineRepo.save(saleLine)
-    ResponseEntity.ok
-      .headers(HeaderUtil.createEntityUpdateAlert("saleLine", saleLine.getId.toString))
-      .body(saleLine)
-  }
 }
