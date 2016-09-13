93d6e8a37fbc31f6f015316b33104d05f3089d24
Kapil Koju <kplx2.0@gmail.com>
2016-09-13 08:17:43 +0545

    Make vehicle search api with number and owner name

--- src/main/scala/io/koju/autopos/party/repo/VehicleRepo.scala
+++ src/main/scala/io/koju/autopos/party/repo/VehicleRepo.scala
@@ -4,3 +4,4 @@ import io.koju.autopos.kernel.service.AuditableBaseRepository
 import io.koju.autopos.party.domain.Vehicle
 
 trait VehicleRepo extends AuditableBaseRepository[Vehicle]
+  with VehicleQuerydslBinderCustomizer
