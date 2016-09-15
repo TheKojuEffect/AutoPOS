0277433d5d733e195553f1cf136dded6516ad62e
Kapil Koju <kplx2.0@gmail.com>
2016-09-15 11:11:52 +0545

    Remove logging and use lombok getter setter
    Closes #80

--- src/main/java/io/koju/autopos/config/JHipsterProperties.java
+++ src/main/java/io/koju/autopos/config/JHipsterProperties.java
@@ -1,18 +1,21 @@
 package io.koju.autopos.config;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.cors.CorsConfiguration;
 
 
 /**
  * Properties specific to JHipster.
- *
  * <p>
- *     Properties are configured in the application.yml file.
+ * <p>
+ * Properties are configured in the application.yml file.
  * </p>
  */
 @ConfigurationProperties(prefix = "jhipster", ignoreUnknownFields = false)
+@Getter
 public class JHipsterProperties {
 
     private final Async async = new Async();
@@ -29,34 +32,8 @@ public class JHipsterProperties {
 
     private final CorsConfiguration cors = new CorsConfiguration();
 
-    public Async getAsync() {
-        return async;
-    }
-
-    public Http getHttp() {
-        return http;
-    }
-
-    public Mail getMail() {
-        return mail;
-    }
-
-    public Security getSecurity() {
-        return security;
-    }
-
-    public Swagger getSwagger() {
-        return swagger;
-    }
-
-    public Metrics getMetrics() {
-        return metrics;
-    }
-
-    public CorsConfiguration getCors() {
-        return cors;
-    }
-
+    @Getter
+    @Setter
     public static class Async {
 
         private int corePoolSize = 2;
@@ -65,82 +42,42 @@ public class JHipsterProperties {
 
         private int queueCapacity = 10000;
 
-        public int getCorePoolSize() {
-            return corePoolSize;
-        }
-
-        public void setCorePoolSize(int corePoolSize) {
-            this.corePoolSize = corePoolSize;
-        }
-
-        public int getMaxPoolSize() {
-            return maxPoolSize;
-        }
-
-        public void setMaxPoolSize(int maxPoolSize) {
-            this.maxPoolSize = maxPoolSize;
-        }
-
-        public int getQueueCapacity() {
-            return queueCapacity;
-        }
-
-        public void setQueueCapacity(int queueCapacity) {
-            this.queueCapacity = queueCapacity;
-        }
     }
 
+    @Getter
     public static class Http {
 
         private final Cache cache = new Cache();
 
-        public Cache getCache() {
-            return cache;
-        }
-
+        @Getter
+        @Setter
         public static class Cache {
 
             private int timeToLiveInDays = 1461;
 
-            public int getTimeToLiveInDays() {
-                return timeToLiveInDays;
-            }
-
-            public void setTimeToLiveInDays(int timeToLiveInDays) {
-                this.timeToLiveInDays = timeToLiveInDays;
-            }
         }
     }
 
+    @Getter
+    @Setter
     public static class Mail {
 
         private String from = "AutoPOS@localhost";
 
-        public String getFrom() {
-            return from;
-        }
-
-        public void setFrom(String from) {
-            this.from = from;
-        }
     }
 
+    @Getter
     public static class Security {
 
         private final Authentication authentication = new Authentication();
 
-        public Authentication getAuthentication() {
-            return authentication;
-        }
-
+        @Getter
         public static class Authentication {
 
             private final Jwt jwt = new Jwt();
 
-            public Jwt getJwt() {
-                return jwt;
-            }
-
+            @Getter
+            @Setter
             public static class Jwt {
 
                 private String secret;
@@ -148,33 +85,12 @@ public class JHipsterProperties {
                 private long tokenValidityInSeconds = 1800;
                 private long tokenValidityInSecondsForRememberMe = 2592000;
 
-                public String getSecret() {
-                    return secret;
-                }
-
-                public void setSecret(String secret) {
-                    this.secret = secret;
-                }
-
-                public long getTokenValidityInSeconds() {
-                    return tokenValidityInSeconds;
-                }
-
-                public void setTokenValidityInSeconds(long tokenValidityInSeconds) {
-                    this.tokenValidityInSeconds = tokenValidityInSeconds;
-                }
-
-                public long getTokenValidityInSecondsForRememberMe() {
-                    return tokenValidityInSecondsForRememberMe;
-                }
-
-                public void setTokenValidityInSecondsForRememberMe(long tokenValidityInSecondsForRememberMe) {
-                    this.tokenValidityInSecondsForRememberMe = tokenValidityInSecondsForRememberMe;
-                }
             }
         }
     }
 
+    @Getter
+    @Setter
     public static class Swagger {
 
         private String title = "AutoPOS API";
@@ -195,79 +111,9 @@ public class JHipsterProperties {
 
         private String licenseUrl;
 
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
-
-        public String getTermsOfServiceUrl() {
-            return termsOfServiceUrl;
-        }
-
-        public void setTermsOfServiceUrl(String termsOfServiceUrl) {
-            this.termsOfServiceUrl = termsOfServiceUrl;
-        }
-
-        public String getContactName() {
-            return contactName;
-        }
-
-        public void setContactName(String contactName) {
-            this.contactName = contactName;
-        }
-
-        public String getContactUrl() {
-            return contactUrl;
-        }
-
-        public void setContactUrl(String contactUrl) {
-            this.contactUrl = contactUrl;
-        }
-
-        public String getContactEmail() {
-            return contactEmail;
-        }
-
-        public void setContactEmail(String contactEmail) {
-            this.contactEmail = contactEmail;
-        }
-
-        public String getLicense() {
-            return license;
-        }
-
-        public void setLicense(String license) {
-            this.license = license;
-        }
-
-        public String getLicenseUrl() {
-            return licenseUrl;
-        }
-
-        public void setLicenseUrl(String licenseUrl) {
-            this.licenseUrl = licenseUrl;
-        }
     }
 
+    @Getter
     public static class Metrics {
 
         private final Jmx jmx = new Jmx();
@@ -278,36 +124,16 @@ public class JHipsterProperties {
 
         private final Logs logs = new Logs();
 
-        public Jmx getJmx() {
-            return jmx;
-        }
-
-        public Spark getSpark() {
-            return spark;
-        }
-
-        public Graphite getGraphite() {
-            return graphite;
-        }
-
-        public Logs getLogs() {
-            return logs;
-        }
-
-
+        @Getter
+        @Setter
         public static class Jmx {
 
             private boolean enabled = true;
 
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
         }
 
+        @Getter
+        @Setter
         public static class Spark {
 
             private boolean enabled = false;
@@ -316,31 +142,10 @@ public class JHipsterProperties {
 
             private int port = 9999;
 
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-
-            public String getHost() {
-                return host;
-            }
-
-            public void setHost(String host) {
-                this.host = host;
-            }
-
-            public int getPort() {
-                return port;
-            }
-
-            public void setPort(int port) {
-                this.port = port;
-            }
         }
 
+        @Getter
+        @Setter
         public static class Graphite {
 
             private boolean enabled = false;
@@ -351,99 +156,16 @@ public class JHipsterProperties {
 
             private String prefix = "AutoPOS";
 
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-
-            public String getHost() {
-                return host;
-            }
-
-            public void setHost(String host) {
-                this.host = host;
-            }
-
-            public int getPort() {
-                return port;
-            }
-
-            public void setPort(int port) {
-                this.port = port;
-            }
-
-            public String getPrefix() {
-                return prefix;
-            }
-
-            public void setPrefix(String prefix) {
-                this.prefix = prefix;
-            }
         }
 
-        public static  class Logs {
+        @Getter
+        @Setter
+        public static class Logs {
 
             private boolean enabled = false;
 
             private long reportFrequency = 60;
 
-            public long getReportFrequency() {
-                return reportFrequency;
-            }
-
-            public void setReportFrequency(int reportFrequency) {
-                this.reportFrequency = reportFrequency;
-            }
-
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
         }
     }
-
-    private final Logging logging = new Logging();
-
-    public Logging getLogging() { return logging; }
-
-    public static class Logging {
-
-        private final Logstash logstash = new Logstash();
-
-        public Logstash getLogstash() { return logstash; }
-
-        public static class Logstash {
-
-            private boolean enabled = false;
-
-            private String host = "localhost";
-
-            private int port = 5000;
-
-            private int queueSize = 512;
-
-            public boolean isEnabled() { return enabled; }
-
-            public void setEnabled(boolean enabled) { this.enabled = enabled; }
-
-            public String getHost() { return host; }
-
-            public void setHost(String host) { this.host = host; }
-
-            public int getPort() { return port; }
-
-            public void setPort(int port) { this.port = port; }
-
-            public int getQueueSize() { return queueSize; }
-
-            public void setQueueSize(int queueSize) { this.queueSize = queueSize; }
-        }
-    }
-
 }
