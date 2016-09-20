24c0c5297d52fc0cbef3f967d2e50e801be3b15a
Kapil Koju <kplx2.0@gmail.com>
2016-09-20 13:03:22 +0545

    Minor Remove obsolete lombok plugin

--- src/main/scala/io/koju/autopos/trade/purchase/domain/PurchaseLine.scala
+++ src/main/scala/io/koju/autopos/trade/purchase/domain/PurchaseLine.scala
@@ -8,14 +8,12 @@ import javax.validation.constraints.{Min, NotNull}
 
 import com.fasterxml.jackson.annotation.JsonBackReference
 import io.koju.autopos.trade.domain.LineItem
-import lombok.{Getter, Setter}
 
 import scala.beans.BeanProperty
 
 @Entity
 @Table(name = "purchase_line")
-@Getter
-@Setter class PurchaseLine extends LineItem {
+class PurchaseLine extends LineItem {
 
   @Id
   @SequenceGenerator(name = "purchase_line_id_seq", sequenceName = "purchase_line_id_seq", allocationSize = 1)
@@ -26,7 +24,8 @@ import scala.beans.BeanProperty
   @NotNull
   @Min(value = 0)
   @Column(name = "rate", precision = 10, scale = 2, nullable = false)
-  @BeanProperty var rate: BigDecimal = _
+  @BeanProperty
+  var rate: BigDecimal = _
 
   @NotNull
   @ManyToOne(optional = false)
