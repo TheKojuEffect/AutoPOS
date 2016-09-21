65b0cee02fb6492f4397cdf27398ae3d72d4c6f0
Kapil Koju <kplx2.0@gmail.com>
2016-09-21 07:15:50 +0545

    Sort the receipts by receipt number

--- src/main/webapp/app/transaction/receipt/receipt.state.js
+++ src/main/webapp/app/transaction/receipt/receipt.state.js
@@ -29,7 +29,7 @@
                     squash: true
                 },
                 sort: {
-                    value: 'id,asc',
+                    value: 'receiptNumber,desc',
                     squash: true
                 },
                 search: null
