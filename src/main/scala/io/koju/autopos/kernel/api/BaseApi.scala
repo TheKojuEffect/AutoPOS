e48b9a1d5d8fd2aa0d8a916a736cbb9b13a0e3a0
Kapil Koju <kplx2.0@gmail.com>
2016-11-05 21:06:53 +0545

    Put Crud api methods into it instead of other helper traits. Wasn't working with Scala 2.12

--- src/main/scala/io/koju/autopos/kernel/api/BaseApi.scala
+++ src/main/scala/io/koju/autopos/kernel/api/BaseApi.scala
@@ -1,13 +1,26 @@
 package io.koju.autopos.kernel.api
 
+import java.util
+
+import com.codahale.metrics.annotation.Timed
 import io.koju.autopos.kernel.domain.AuditableBaseEntity
 import io.koju.autopos.kernel.service.AuditableBaseRepository
-import org.springframework.http.MediaType
+import io.koju.autopos.web.rest.util.PaginationUtil
+import org.springframework.data.domain.Pageable
+import org.springframework.http.{HttpStatus, MediaType, ResponseEntity}
 import org.springframework.web.bind.annotation._
 
 @RequestMapping(produces = Array(MediaType.APPLICATION_JSON_VALUE))
 abstract class BaseApi[T <: AuditableBaseEntity](override protected val repo: AuditableBaseRepository[T],
                                                  override protected val entityName: String,
                                                  override protected val baseUrl: String)
-  extends CrudApi[T](repo, entityName, baseUrl)
-    with GetAllApi[T]
+  extends CrudApi[T](repo, entityName, baseUrl) {
+
+  @GetMapping
+  @Timed
+  def getAll(pageable: Pageable): ResponseEntity[util.List[T]] = {
+    val page = repo.findAll(pageable)
+    val headers = PaginationUtil.generatePaginationHttpHeaders(page, baseUrl)
+    new ResponseEntity(page.getContent, headers, HttpStatus.OK)
+  }
+}
