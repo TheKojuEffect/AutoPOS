6c07572e019645ea9b97a66d7f6f7a7ddadc330e
Kapil Koju <kplx2.0@gmail.com>
2016-12-11 16:43:04 +0545

    Revert the use of jquery explicitly

--- src/app/components/login/login.controller.js
+++ src/app/components/login/login.controller.js
@@ -1,71 +1,67 @@
+import * as angular from 'angular';
 
+angular
+    .module('autopos')
+    .controller('LoginController', LoginController);
 
-(function ($) {
-    'use strict';
+LoginController.$inject = ['$rootScope', '$state', '$timeout', 'Auth', '$uibModalInstance'];
 
-    angular
-        .module('autopos')
-        .controller('LoginController', LoginController);
+function LoginController($rootScope, $state, $timeout, Auth, $uibModalInstance) {
+    const vm = this;
 
-    LoginController.$inject = ['$rootScope', '$state', '$timeout', 'Auth', '$uibModalInstance'];
+    vm.authenticationError = false;
+    vm.cancel = cancel;
+    vm.credentials = {};
+    vm.login = login;
+    vm.password = null;
+    vm.register = register;
+    vm.rememberMe = true;
+    vm.requestResetPassword = requestResetPassword;
+    vm.username = null;
 
-    function LoginController($rootScope, $state, $timeout, Auth, $uibModalInstance) {
-        var vm = this;
+    $timeout(function () {
+        angular.element('[ng-model="vm.username"]').focus();
+    });
 
+    function cancel() {
+        vm.credentials = {
+            username: null,
+            password: null,
+            rememberMe: true
+        };
         vm.authenticationError = false;
-        vm.cancel = cancel;
-        vm.credentials = {};
-        vm.login = login;
-        vm.password = null;
-        vm.register = register;
-        vm.rememberMe = true;
-        vm.requestResetPassword = requestResetPassword;
-        vm.username = null;
-
-        $timeout(function () {
-            $('[ng-model="vm.username"]').focus();
-        });
-
-        function cancel() {
-            vm.credentials = {
-                username: null,
-                password: null,
-                rememberMe: true
-            };
-            vm.authenticationError = false;
-            $uibModalInstance.dismiss('cancel');
-        }
-
-        function login(event) {
-            event.preventDefault();
-            Auth.login({
-                username: vm.username,
-                password: vm.password,
-                rememberMe: vm.rememberMe
-            }).then(function () {
-                vm.authenticationError = false;
-                $uibModalInstance.close();
-                // If we're redirected to login, our
-                // previousState is already set in the authExpiredInterceptor. When login succesful go to stored state
-                if ($rootScope.redirected && $rootScope.previousStateName) {
-                    $state.go($rootScope.previousStateName, $rootScope.previousStateParams);
-                    $rootScope.redirected = false;
-                } else {
-                    $rootScope.$broadcast('authenticationSuccess');
-                }
-            }).catch(function () {
-                vm.authenticationError = true;
-            });
-        }
-
-        function register() {
-            $uibModalInstance.dismiss('cancel');
-            $state.go('register');
-        }
-
-        function requestResetPassword() {
-            $uibModalInstance.dismiss('cancel');
-            $state.go('requestReset');
-        }
+        $uibModalInstance.dismiss('cancel');
     }
-})(require('jquery'));
+
+    function login(event) {
+        event.preventDefault();
+        Auth.login({
+            username: vm.username,
+            password: vm.password,
+            rememberMe: vm.rememberMe
+        }).then(function () {
+            vm.authenticationError = false;
+            $uibModalInstance.close();
+            // If we're redirected to login, our
+            // previousState is already set in the authExpiredInterceptor. When login succesful go to stored state
+            if ($rootScope.redirected && $rootScope.previousStateName) {
+                $state.go($rootScope.previousStateName, $rootScope.previousStateParams);
+                $rootScope.redirected = false;
+            } else {
+                $rootScope.$broadcast('authenticationSuccess');
+            }
+        }).catch(function () {
+            vm.authenticationError = true;
+        });
+    }
+
+    function register() {
+        $uibModalInstance.dismiss('cancel');
+        $state.go('register');
+    }
+
+    function requestResetPassword() {
+        $uibModalInstance.dismiss('cancel');
+        $state.go('requestReset');
+    }
+}
