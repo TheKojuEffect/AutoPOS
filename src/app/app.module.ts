0dc939270d1462970bbbaef5b22135ea356a449d
Kapil Koju <kplx2.0@gmail.com>
2016-12-11 13:29:59 +0545

    Remove unnecessary angular2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import { UpgradeModule } from '@angular/upgrade/static';
-import {HttpModule} from '@angular/http';
-import {FormsModule} from '@angular/forms';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {ApiService} from './shared';
-import {routing} from './app.routing';
+import {UpgradeModule} from '@angular/upgrade/static';
 
 @NgModule({
     imports: [
         BrowserModule,
-        UpgradeModule,
-        HttpModule,
-        FormsModule,
-        routing
-    ],
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        AboutComponent
-    ],
-    providers: [
-        ApiService
+        UpgradeModule
     ]
 })
 export class AppModule {
-    ngDoBootstrap() {}
+    ngDoBootstrap() {
+    }
 }
