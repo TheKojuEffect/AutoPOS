e814e168e0efea6a7e9f78233407bc447fd00c05
Kapil Koju <kplx2.0@gmail.com>
2017-03-11 10:12:16 +0545

    Remove unnecessary annotations

--- src/main/scala/com/kapilkoju/autopos/config/MetricsConfiguration.scala
+++ src/main/scala/com/kapilkoju/autopos/config/MetricsConfiguration.scala
@@ -1,24 +1,20 @@
 package com.kapilkoju.autopos.config
 
-import io.github.jhipster.config.JHipsterProperties
-import com.codahale.metrics.JmxReporter
-import com.codahale.metrics.MetricRegistry
-import com.codahale.metrics.Slf4jReporter
-import com.codahale.metrics.health.HealthCheckRegistry
-import com.codahale.metrics.jvm._
-import com.ryantenney.metrics.spring.config.annotation.EnableMetrics
-import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter
-import com.zaxxer.hikari.HikariDataSource
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation._
-import javax.annotation.PostConstruct
 import java.lang.management.ManagementFactory
 import java.util.concurrent.TimeUnit
+import javax.annotation.PostConstruct
 
-@Configuration
-@EnableMetrics(proxyTargetClass = true) object MetricsConfiguration {
+import com.codahale.metrics.health.HealthCheckRegistry
+import com.codahale.metrics.jvm._
+import com.codahale.metrics.{JmxReporter, MetricRegistry, Slf4jReporter}
+import com.ryantenney.metrics.spring.config.annotation.{EnableMetrics, MetricsConfigurerAdapter}
+import com.zaxxer.hikari.HikariDataSource
+import io.github.jhipster.config.JHipsterProperties
+import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation._
+
+object MetricsConfiguration {
   private val PROP_METRIC_REG_JVM_MEMORY: String = "jvm.memory"
   private val PROP_METRIC_REG_JVM_GARBAGE: String = "jvm.garbage"
   private val PROP_METRIC_REG_JVM_THREADS: String = "jvm.threads"
@@ -27,25 +23,33 @@ import java.util.concurrent.TimeUnit
 }
 
 @Configuration
-@EnableMetrics(proxyTargetClass = true) class MetricsConfiguration(val jHipsterProperties: JHipsterProperties) extends MetricsConfigurerAdapter {
+@EnableMetrics(proxyTargetClass = true)
+class MetricsConfiguration(val jHipsterProperties: JHipsterProperties)
+  extends MetricsConfigurerAdapter {
+
   final private val log: Logger = LoggerFactory.getLogger(classOf[MetricsConfiguration])
+
   private val metricRegistry: MetricRegistry = new MetricRegistry
   private val healthCheckRegistry: HealthCheckRegistry = new HealthCheckRegistry
   private var hikariDataSource: HikariDataSource = null
 
-  @Autowired(required = false) def setHikariDataSource(hikariDataSource: HikariDataSource) {
+  @Autowired(required = false)
+  def setHikariDataSource(hikariDataSource: HikariDataSource) {
     this.hikariDataSource = hikariDataSource
   }
 
-  @Bean override def getMetricRegistry: MetricRegistry = {
-    return metricRegistry
+  @Bean
+  override def getMetricRegistry: MetricRegistry = {
+    metricRegistry
   }
 
-  @Bean override def getHealthCheckRegistry: HealthCheckRegistry = {
-    return healthCheckRegistry
+  @Bean
+  override def getHealthCheckRegistry: HealthCheckRegistry = {
+    healthCheckRegistry
   }
 
-  @PostConstruct def init() {
+  @PostConstruct
+  def init() {
     log.debug("Registering JVM gauges")
     metricRegistry.register(MetricsConfiguration.PROP_METRIC_REG_JVM_MEMORY, new MemoryUsageGaugeSet)
     metricRegistry.register(MetricsConfiguration.PROP_METRIC_REG_JVM_GARBAGE, new GarbageCollectorMetricSet)
