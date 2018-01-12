c67c75463b706582a71736daf288648f0c01c8f0
Kapil Koju <kplx2.0@gmail.com>
2018-01-12 15:08:36 +0545

    Do not make Purchase extend Trade

--- src/main/java/com/kapilkoju/autopos/trade/purchase/domain/Purchase.java
+++ src/main/java/com/kapilkoju/autopos/trade/purchase/domain/Purchase.java
@@ -2,20 +2,24 @@ package com.kapilkoju.autopos.trade.purchase.domain;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.kapilkoju.autopos.kernel.domain.AuditableEntity;
 import com.kapilkoju.autopos.kernel.json.Views;
 import com.kapilkoju.autopos.party.domain.Vendor;
 import com.kapilkoju.autopos.trade.domain.LineItem;
-import com.kapilkoju.autopos.trade.domain.Trade;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Entity
 @Table(name = "purchase")
-public class Purchase extends Trade {
+public class Purchase extends AuditableEntity {
 
     @ManyToOne
     @JoinColumn(name = "vendor_id")
@@ -25,6 +29,25 @@ public class Purchase extends Trade {
     @OneToMany(mappedBy = "purchase", fetch = FetchType.EAGER)
     @JsonManagedReference
     private List<PurchaseLine> lines = new ArrayList<>();
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    @JsonView(Views.Summary.class)
+    private Instant date;
+
+    @NotNull
+    @Min(value = 0)
+    @Column(name = "discount", precision = 10, scale = 2, nullable = false)
+    private BigDecimal discount = BigDecimal.ZERO;
+
+    @Size(min = 1, max = 50)
+    @Column(name = "invoice_number", length = 50)
+    @JsonView(Views.Summary.class)
+    private String invoiceNumber;
+
+    @Size(max = 250)
+    @Column(name = "remarks", length = 250)
+    private String remarks;
 
     public Vendor getVendor() {
         return vendor;
@@ -40,6 +63,38 @@ public class Purchase extends Trade {
 
     public void setLines(List<PurchaseLine> lines) {
         this.lines = lines;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
     }
 
     @JsonView(Views.Summary.class)
