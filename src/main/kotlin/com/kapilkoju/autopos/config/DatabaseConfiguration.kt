0d2fa267d494f18d792e845b351db67224def20d
Kapil Koju <kplx2.0@gmail.com>
2018-01-16 21:47:48 +0545

    Remove redundant EntityScan and EnableJpaRepositories

--- src/main/kotlin/com/kapilkoju/autopos/config/DatabaseConfiguration.kt
+++ src/main/kotlin/com/kapilkoju/autopos/config/DatabaseConfiguration.kt
@@ -1,35 +1,12 @@
 package com.kapilkoju.autopos.config
 
-import com.kapilkoju.autopos.accounting.domain.AccountingDomainPackage
-import com.kapilkoju.autopos.accounting.service.AccountingServicePackage
-import com.kapilkoju.autopos.catalog.domain.CatalogDomainPackage
-import com.kapilkoju.autopos.catalog.service.CatalogServicePackage
 import com.kapilkoju.autopos.config.liquibase.AsyncSpringLiquibase
-import com.kapilkoju.autopos.domain.DomainPackage
-import com.kapilkoju.autopos.kernel.domain.AbstractEntity
-import com.kapilkoju.autopos.party.domain.PartyDomainPackage
-import com.kapilkoju.autopos.party.service.PartyServicePackage
-import com.kapilkoju.autopos.repository.RepositoryPackage
 import com.kapilkoju.autopos.security.SecurityUtils
-import com.kapilkoju.autopos.shared.domain.SharedDomainPackage
-import com.kapilkoju.autopos.shared.service.SharedServicePackage
-import com.kapilkoju.autopos.stockbook.domain.StockBookDomainPackage
-import com.kapilkoju.autopos.stockbook.service.StockBookServicePackage
-import com.kapilkoju.autopos.trade.domain.TradeDomainPackage
-import com.kapilkoju.autopos.trade.purchase.domain.PurchaseDomainPackage
-import com.kapilkoju.autopos.trade.purchase.service.PurchaseServicePackage
-import com.kapilkoju.autopos.trade.sale.domain.SaleDomainPackage
-import com.kapilkoju.autopos.trade.sale.service.SaleServicePackage
-import com.kapilkoju.autopos.trade.service.TradeServicePackage
-import com.kapilkoju.autopos.transaction.domain.TransactionDomainPackage
-import com.kapilkoju.autopos.transaction.service.TransactionServicePackage
 import com.kapilkoju.autopos.user.domain.User
-import com.kapilkoju.autopos.user.service.UserServicePackage
 import liquibase.integration.spring.SpringLiquibase
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -37,42 +14,9 @@ import org.springframework.core.env.Environment
 import org.springframework.core.task.TaskExecutor
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-        basePackageClasses = [
-            RepositoryPackage::class,
-            SharedServicePackage::class,
-            CatalogServicePackage::class,
-            UserServicePackage::class,
-            AccountingServicePackage::class,
-            PartyServicePackage::class,
-            TradeServicePackage::class,
-            SaleServicePackage::class,
-            PurchaseServicePackage::class,
-            TransactionServicePackage::class,
-            CatalogServicePackage::class,
-            StockBookServicePackage::class
-        ])
-@EntityScan(
-        basePackageClasses = [
-            AbstractEntity::class,
-            DomainPackage::class,
-            SharedDomainPackage::class,
-            User::class,
-            CatalogDomainPackage::class,
-            AccountingDomainPackage::class,
-            PartyDomainPackage::class,
-            TradeDomainPackage::class,
-            SaleDomainPackage::class,
-            PurchaseDomainPackage::class,
-            TransactionDomainPackage::class,
-            StockBookDomainPackage::class]
-)
 @EnableJpaAuditing
 class DatabaseConfiguration(private val env: Environment) {
 
